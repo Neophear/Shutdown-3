e3a5c5910dc325c8e3c9aeded6c23e5258172950
Neophear <stiiggade@gmail.com>
2019-10-11 10:43:27 +0200

    Initial commit

---
+++ Shutdown 3/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Stiig;
+
+namespace Shutdown_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : CustomWindow
+    {
+        public TimeSpan spn { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            spn = DateTime.Now.TimeOfDay;
+            DataContext = this;
+        }
+    }
+}
